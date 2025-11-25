98382349f08adf64c16eca7f6b23b5677ddb71d0
AliciaJAL <ajoyaland@gmail.com>
2025-11-25 01:08:35 -0600

    Fixed typo

--- js/ghostMovement.js
+++ js/ghostMovement.js
@@ -149,7 +149,7 @@ function preload() {
   npcRight = loadImage("assets/npcRight.png");
   npcDown = loadImage("assets/npcDown.png");
   npcLeft = loadImage("assets/npcLeft.png");
-  soul = loadImage("assests/soul.png")
+  soul = loadImage("assets/soul.png")
 
   menuBg = loadImage("assets/cemetery_bg.png");
   cityBg = loadImage("assets/city_bg.png");
